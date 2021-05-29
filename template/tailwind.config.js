6a5b64ab43565bf8fdd2995fca15989d2a603e7f
Mohamed Snoussi <sooniic@live.com>
2021-05-29 16:24:15 +0200

    adding more color to the tailwind config

--- template/tailwind.config.js
+++ template/tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     colors:{
       primary: "#845EC2",
       secondary: "#4B4453",
+      black: "#2a2a2e",
+      blackLighter: "#38383d",
+      blueFire: "#007de6",
+      blueAis: "#3237a7",
       accent: "#B0A8B9"
     },
     extend: {},
