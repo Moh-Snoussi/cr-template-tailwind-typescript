f95ab472bb7a7061562094ec6ee9f99411076913
Mohamed Snoussi <sooniic@live.com>
2021-06-10 12:06:05 +0200

    cleaning the test file

--- template/src/Tests/App.test.tsx
+++ template/src/Tests/App.test.tsx
@@ -1,33 +1,9 @@
 import '@testing-library/jest-dom';
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import App from '../Components/App';
-import Api from "../Commons/Api"
 
-const api = new Api()
-const mock = (api.apiEndPoint = jest.fn())
+describe('test $', () => {
+    it('should $', () => {
 
-test('renders learn react link', () => {
-  render(<App  testProp="hello"/>);
-  const linkElement = screen.getByText(/Hello/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-
-test('Clicking on it change the content', () => {
-  render(<App  testProp="hello"/>);
-  const linkElement = screen.getByText(/start/i);
-
-  fireEvent.click(linkElement)
-  expect(linkElement).toHaveTextContent(/hello/i);
-});
-
-test('Api caller', () => {
-
-  mock.mockReturnValue("never Know")
-  render(<App  testProp="hello"/>);
-  const linkElement = screen.getByText(/start/i);
-
-  fireEvent.click(linkElement)
-  expect(linkElement).toHaveTextContent(/never Know/i);
+    })
 })
